015f0166986a42ab1a6bbf3144e1eb3d423ad0e1
Lauren Spencer <spencer.lauren.m@gmail.com>
2015-02-13 09:23:11 +0000

    css additions

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   resources :games
   resources :moves
 
+  get 'users/games' => 'games#user_games', as: 'user_games' #custom path
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'games#index'
+  root 'games#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
